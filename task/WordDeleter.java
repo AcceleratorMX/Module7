827efb56411661d5930beb54c8affd131c081bd6
AcceleratorMX <redfield242@gmail.com>
2022-11-27 15:23:23 +0200

    fi

---
+++ task/WordDeleter.java
@@ -0,0 +1,20 @@
+package task;
+
+public class WordDeleter {
+    public static void main(String[] args) {
+        System.out.println(new WordDeleter().remove("Hello Java", new String[]{"Java"}));
+    }
+
+    public String remove(String phrase, String[] words){
+        String[] strings = phrase.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (!strings[i].equals(words[j])){
+                    builder.append(strings[i]);
+                }
+            }
+        }
+        return builder.toString();
+    }
+}
