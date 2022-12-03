a8a48c0fbe795693ceadad322cc8bd0768eaf49d
AcceleratorMX <redfield242@gmail.com>
2022-12-03 11:52:54 +0200

    Module 7

--- src/homeWork/h20/BigOrSmall.java
+++ src/homeWork/h20/BigOrSmall.java
@@ -1,21 +1,21 @@
 package src.homeWork.h20;
 
 class BigOrSmall {
-    public String calculate(String text) {
-        text.replaceAll(" ", "");
-        int up = 0;
-        int low = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (Character.isUpperCase(text.charAt(i))) up++;
-            if (Character.isLowerCase(text.charAt(i))) low++;
+    public String calculate(String text) {                    //Приймає рядок із тестів, що внизу.
+        text.replaceAll(" ", "");            //Видаляє з рядку усі пробіли.
+        int up = 0;                                           //Змінна, в яку вноситься кількість великих літер.
+        int low = 0;                                          //Змінна, в яку вноситься кількість маленьких літер.
+        for (int i = 0; i < text.length(); i++) {             //Цикл, який перевіряє кожен символ рядка.
+            if (Character.isUpperCase(text.charAt(i))) up++;  //Якщо цикл знаходить велику літеру, то відповідна змінна збільшується на 1.
+            if (Character.isLowerCase(text.charAt(i))) low++; //Якщо цикл знаходить маленьку літеру, то відповідна змінна збільшується на 1.
         }
-        if (up < low) return "Small";
-        if (up > low) return "Big";
-        if (up == low) return "Same";
+        if (up < low) return "Small";                         //Якщо меленьких літер більше ніж великих, виводить "Small".
+        if (up > low) return "Big";                           //Якщо великих літер більше ніж маленьких, виводить "Big".
+        if (up == low) return "Same";                         //Якщо кількість великих і маленьких літер однакова, виводить "Same".
         return null;
     }
 }
-
+                                                            //Нижче тестові варіанти рядків для цього завдання.
 class BigOrSmallTest {
     public static void main(String[] args) {
         //Small
